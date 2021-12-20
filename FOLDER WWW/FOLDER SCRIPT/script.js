b4fd9061b79a4816f6b8a530f8c0b67337b8c3a3
Majdzel <92318639+Majdzel@users.noreply.github.com>
2021-12-20 19:35:57 +0100

    add map view

--- FOLDER WWW/FOLDER SCRIPT/script.js
+++ FOLDER WWW/FOLDER SCRIPT/script.js
@@ -1,9 +1,36 @@
 function mode() {
     let body = document.body;
-    let topL = document.getElementById("topL")
-    let topR = document.getElementById("topR")
-
     body.classList.toggle("dark-mode");
-    topL.classList.toggle("topL-dark");
-    topR.classList.toggle("topR-dark");
   }
+
+require([
+  'esri/Map',
+  'esri/views/MapView',
+  'dijit/from/Button'
+],(Map,MapView,Button) => {
+  
+  const map1 = new Map({
+    basemap: "topo"
+  });
+
+  const view = new MapView({
+    map: map1,
+    container: "divMap",
+    zoom: 12,
+    center: [25,52]
+  });
+
+  const zoomIn = new Button({
+    onClick: () => {
+      view.zoom = view.zoom + 1 ;
+    }
+  },"zoomIn");
+
+  const zoomOut = new Button({
+
+    onClick: () =>{
+      view.zoom = view.zoom -1;
+    }
+  },"zoomOut");
+
+});
